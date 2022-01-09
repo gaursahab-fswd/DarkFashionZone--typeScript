cfb9025274124d8a09fd26b78d9a3f37bb0e3fbc
itprogaur <professionalcodehub@gmail.com>
2022-01-09 19:01:53 +0530

    Initialize typeScript Project

---
+++ src/Fullpage/Components/Header.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './cover.jpg'
+
+const HeaderSection = () =>{
+
+    return(
+    <div className='container'>
+        <div className='container flex'>
+            <img src='{cover.jpg}' alt='background-cover-image'/>
+        </div>
+    </div>
+);
+}
+
+export default HeaderSection;
