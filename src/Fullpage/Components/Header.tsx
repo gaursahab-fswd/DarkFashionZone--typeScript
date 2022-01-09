f0a53562f0e3867afb0b6347b1f07b5019d8357c
itprogaur <professionalcodehub@gmail.com>
2022-01-09 20:14:28 +0530

    Header Completed with Tailwind CSS

--- src/Fullpage/Components/Header.tsx
+++ src/Fullpage/Components/Header.tsx
@@ -1,12 +1,17 @@
 import React from 'react'
-import './cover.jpg'
+import  './Component.style.css'
+
 
 const HeaderSection = () =>{
 
     return(
     <div className='container'>
-        <div className='container flex'>
-            <img src='{cover.jpg}' alt='background-cover-image'/>
+        <div className='container home-background flex bg-gray-900 text-center '>
+            <div className='mx-auto pt-72'> 
+                <h1 className='text-7xl text-white'>Dark Fashion Zone</h1>
+                <h1 className=' text-xl py-8 text-gray-400 uppercase'>A way to dark premiumness.</h1>
+                <button className='rounded bg-zinc-800	 p-2 px-8 text-white hover:bg-zinc-400'>FIND MORE</button>
+            </div>
         </div>
     </div>
 );
